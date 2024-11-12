96c600efcbae9904dc17c5ed21cc53b720a782d0
Sumit <sumitprasadarya392@gmail.com>
2024-11-12 14:55:54 +0530

    api key solved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,11 @@ export const GET_MOVIE_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDQ4ZjUwZjRiNmY5OThhYjlmYTEwZDBhYmEzNWY5YSIsIm5iZiI6MTcyODc2MTEwNi4zNDkyNTgsInN1YiI6IjY3MGFjMDNiMzdkODZkNTIwYmIwOTFmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ssESrSDYYuiBw1TcsQk8g1wUr4vpEOFToFXxXYi070Q",
   },
 };
 
 export const GET_IMG_URL = "https://image.tmdb.org/t/p/w500";
 
-export const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+export const OPENAI_API_KEY = "AIzaSyDrx7q9Nq2CTeGheoirYYbcxrbzSaxGTEU";
